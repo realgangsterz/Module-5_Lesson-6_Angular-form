173382740e47ff9963e637b9ae350e0f33b6cbf2
daoxuandong1997 <daoxuandong1997@gmail.con>
2020-09-28 08:46:03 +0700

    hoan thanh binding du lieu [ngModel]

---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.scss']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+
+  profile = {
+    username: 'Dong',
+    email: 'dao.xuan.dong.212@gmail.com',
+    facebook: 'facebook.com',
+    twitter: 'twitter.com',
+    website: 'daoxuandong.com',
+    tel: '0964224490'
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  onSubmit(profileForm) {
+    console.log(profileForm);
+  }
+}
